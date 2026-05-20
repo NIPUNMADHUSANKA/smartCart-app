d63f60fe95c45e5a15369a3347cc3a07b28acb80
NIPUNMADHUSANKA <nipunmadhusanka201080@gmail.com>
2026-05-21 00:40:21 +0530

    implemt struct

---
+++ models/category_model.go
@@ -0,0 +1,69 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type CategoryStatus string
+type ItemStatus string
+type PriorityStatus string
+type UnitStatus string
+
+const (
+	CategoryStatusActive    CategoryStatus = "active"
+	CategoryStatusCompleted CategoryStatus = "completed"
+)
+const (
+	ItemStatusActive    ItemStatus = "active"
+	ItemStatusCompleted ItemStatus = "archived"
+)
+
+const (
+	PriorityStatusNormal PriorityStatus = "normal"
+	PriorityStatusLow    PriorityStatus = "low"
+	PriorityStatusMedium PriorityStatus = "medium"
+	PriorityStatusHigh   PriorityStatus = "high"
+)
+
+const (
+	Kg     UnitStatus = "kg"
+	Piece  UnitStatus = "piece"
+	Pack   UnitStatus = "pack"
+	Dozen  UnitStatus = "dozen"
+	Box    UnitStatus = "box"
+	G      UnitStatus = "g"
+	L      UnitStatus = "l"
+	Ml     UnitStatus = "ml"
+	Bottle UnitStatus = "bottle"
+	Can    UnitStatus = "can"
+	Cup    UnitStatus = "cup"
+	Other  UnitStatus = "other"
+)
+
+type ShoppingItem struct {
+	ItemId      uuid.UUID      `json:"item_id" validate:"required"`
+	ItemName    string         `json:"item_name" validate:"required"`
+	Description string         `json:"description" validate:"required,min=2,max=500"`
+	Status      ItemStatus     `json:"status" validate:"oneof=active archived"`
+	CategoryId  string         `json:"category_id" validate:"required"`
+	Priority    PriorityStatus `json:"priority" validate:"oneof=normal low medium high"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updateded_at"`
+	Quantity    int            `json:"quantity" validate:"required"`
+	Unit        UnitStatus     `json:"unit" validate:"required"`
+}
+
+type Category struct {
+	CategoryId    uuid.UUID      `json:"category_id" validate:"required"`
+	CategoryName  string         `json:"category_name" validate:"required"`
+	Description   string         `json:"description" validate:"required,min=2,max=500"`
+	Status        CategoryStatus `json:"status" validate:"oneof=active completed"`
+	UserId        string         `json:"user_id" validate:"required"`
+	Icon          string         `json:"icon" validate:"required"`
+	Priority      PriorityStatus `json:"priority" validate:"oneof=normal low medium high"`
+	CreatedAt     time.Time      `json:"created_at"`
+	UpdatedAt     time.Time      `json:"updateded_at"`
+	ShoppingItems []ShoppingItem `json:"shopping_items" validate:"required,dive"`
+}
---
+++ models/user_model.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	UserID       uuid.UUID `json:"user_id"`
+	FullName     string    `json:"full_name" validate:"required,min=2,max=100"`
+	UserName     string    `json:"user_name" validate:"required,min=2,max=100"`
+	Email        string    `json:"email" validate:"required,email"`
+	Password     string    `json:"password" validate:"required,min=6"`
+	Role         string    `json:"role" validate:"oneof=ADMIN USER"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updateded_at"`
+	Token        string    `json:"token"`
+	RefreshToken string    `json:"refresh_token"`
+}
+
+type UserLogin struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type UserResponse struct {
+	UserId       string `json:"user_id"`
+	FullName     string `json:"full_name"`
+	Email        string `json:"email"`
+	Role         string `json:"role"`
+	Token        string `json:"token"`
+	RefreshToken string `json:"refresh_token"`
+}
